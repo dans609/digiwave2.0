211a2f89a7e8f473294bc0647236cad8bea69db1
Dandi Anastasa <dandianastasa12@gmail.com>
2022-04-30 16:20:43 +0700

    remove explicit Dispacthers.DEFAULT type of CoroutineScope

--- app/src/main/java/com/dash/projects/android/digiwave/views/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/dash/projects/android/digiwave/views/splash/SplashScreenActivity.kt
@@ -2,16 +2,12 @@ package com.dash.projects.android.digiwave.views.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.dash.projects.android.digiwave.R
 import com.dash.projects.android.digiwave.views.home.MainActivity
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -20,7 +16,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleScope.launch(Dispatchers.Default) {
+        lifecycleScope.launch {
             try {
                 delay(resources.getInteger(R.integer.milliSecs_3000).toLong())
             } catch (e: InterruptedException) {
