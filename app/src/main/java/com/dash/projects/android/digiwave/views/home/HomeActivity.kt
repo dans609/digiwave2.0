fc6bb65ba5753ac2be2d8f7b18764942c25fd947
Dandi Anastasa <dandianastasa12@gmail.com>
2022-05-03 18:40:57 +0700

    change home screen layout name and get ready for designing home screen

--- app/src/main/java/com/dash/projects/android/digiwave/views/home/HomeActivity.kt
+++ app/src/main/java/com/dash/projects/android/digiwave/views/home/HomeActivity.kt
@@ -2,18 +2,18 @@ package com.dash.projects.android.digiwave.views.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.dash.projects.android.digiwave.databinding.ActivityMainBinding
+import com.dash.projects.android.digiwave.databinding.ActivityHomeBinding
 import com.dash.projects.android.digiwave.enum.FeatureName
 import com.dash.projects.android.digiwave.interfaces.OnFeatureClickCallback
 
 class HomeActivity : AppCompatActivity(), OnFeatureClickCallback {
-    private var _binding: ActivityMainBinding? = null
+    private var _binding: ActivityHomeBinding? = null
     private val binding
         get() = _binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ::_binding.set(ActivityMainBinding.inflate(layoutInflater))
+        ::_binding.set(ActivityHomeBinding.inflate(layoutInflater))
         setContentView(binding?.root)
     }
 
@@ -22,12 +22,8 @@ class HomeActivity : AppCompatActivity(), OnFeatureClickCallback {
         _binding = null
     }
 
-    companion object {
-        private const val TAG = "MainActivity"
-    }
-
     override fun onFeatureClicked(featureName: FeatureName) {
-        when(featureName) {
+        when (featureName) {
             FeatureName.KarnaughMap -> {}
             FeatureName.NumberSystem -> {}
             FeatureName.LogicGate -> {}
