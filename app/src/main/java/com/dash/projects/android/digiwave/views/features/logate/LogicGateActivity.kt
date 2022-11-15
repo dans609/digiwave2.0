89201d5ee43402b1e7062c450b0b9422f0ae157d
Dandi Anastasa <dandianastasa12@gmail.com>
2022-11-15 22:47:02 +0700

    create new empty activity as a place for Logic Gate Simulator feature

---
+++ app/src/main/java/com/dash/projects/android/digiwave/views/features/logate/LogicGateActivity.kt
@@ -0,0 +1,16 @@
+package com.dash.projects.android.digiwave.views.features.logate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dash.projects.android.digiwave.databinding.ActivityLogicGateBinding
+
+class LogicGateActivity : AppCompatActivity() {
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityLogicGateBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
